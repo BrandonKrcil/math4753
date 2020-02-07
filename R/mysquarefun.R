de6801533a9b34710d0ce1047ce6c05a5ffcc56e
Brandon <brandon.a.krcil@ou.edu>
2020-02-07 17:08:27 -0600

    clearing commits

---
+++ R/mysquarefun.R
@@ -0,0 +1,14 @@
+#' Square Function
+#'
+#' Takes a vector an returns a vector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; mysquarefun(x)
+squarefun=function(x){
+  x^2
+}
